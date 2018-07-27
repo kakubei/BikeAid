5ee71205baa1d38dffba74c8a97d5e8fa2de796a
Alex Dearden <alex.dearden@farepilot.com>
2018-07-28 00:14:29 +0100

    Cleanup

--- BikeAid/View Controllers/MainViewController.swift
+++ BikeAid/View Controllers/MainViewController.swift
@@ -22,19 +22,10 @@ class MainViewController: UIViewController {
 
      
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func mainButtonTapped(_ sender: CircularButton) {
+        if pumpButton.isHidden == false { return }
         ancillaryButtons.forEach { $0.show() }
     }
     
