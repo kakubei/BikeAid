31b6a578b1296c6f0af249cdeecf902034181889
Alex Dearden <alex.dearden@farepilot.com>
2018-10-01 10:29:21 +0100

    Google Map working.

--- BikeAid/View Controllers/MapViewController.swift
+++ BikeAid/View Controllers/MapViewController.swift
@@ -12,10 +12,13 @@ import GooglePlaces
 
 class MapViewController: UIViewController {
     
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    
     // TODO: Put all these into a model
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation?
-    var mapView: GMSMapView!
     var placesClient: GMSPlacesClient!
     var zoomLevel: Float = 15.0
     
@@ -26,7 +29,7 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.configureMap()
+        configureMap()
     }
 
     private func configureMap() {
@@ -62,31 +65,31 @@ class MapViewController: UIViewController {
         })
     }
 
+    func showMarker(position: CLLocationCoordinate2D){
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = "Somewhere"
+        marker.snippet = "In the UK?"
+        marker.map = mapView
+    }
+    
 }
 
 extension MapViewController: CLLocationManagerDelegate {
-    
+
     // Handle incoming location events.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location: CLLocation = locations.last!
         print("Location: \(location)")
+
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: zoomLevel)
         
-        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
-                                              longitude: location.coordinate.longitude,
-                                              zoom: zoomLevel)
-        
-        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        
-        if mapView.isHidden {
-            mapView.isHidden = false
-            mapView.camera = camera
-        } else {
-            mapView.animate(to: camera)
-        }
-        
-        listLikelyPlaces()
+        mapView.camera = camera
+        showMarker(position: camera.target)
+
+//        listLikelyPlaces()
     }
-    
+
     // Handle authorization for the location manager.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
@@ -103,10 +106,24 @@ extension MapViewController: CLLocationManagerDelegate {
             print("Location status is OK.")
         }
     }
-    
+
     // Handle location manager errors.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         locationManager.stopUpdatingLocation()
         print("Error: \(error)")
+    }
+}
+
+extension MapViewController: GMSMapViewDelegate {
+    
+    //MARK - GMSMarker Dragging
+    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+        print("didBeginDragging")
+    }
+    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
+        print("didDrag")
+    }
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        print("didEndDragging")
     }
 }
